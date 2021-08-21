334ac9bdba92bfae63f9429b38a61610ab11418d
picnicrage <picnicrage@gmail.com>
2021-08-21 15:51:59 +0200

    After implementing car spawning/destryoing and car movement

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    private float speed;
+    
+    // Start is called before the first frame update
+    void Start() {
+        if (transform.position.x < 0) speed = 2f;
+        else speed = -3.25f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        if(transform.position.x > 12 || transform.position.x < -12) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    [SerializeField] private GameObject[] cars = null;
+
+    private float carSpawnTimer;
+    // Start is called before the first frame update
+    void Start() {
+        carSpawnTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time - carSpawnTimer > 1.5f) spawnCars();
+    }
+
+    private void spawnCars() {
+        Instantiate(cars[0], new Vector3(-11f, -1.15f, 0f), Quaternion.identity);
+        Instantiate(cars[1], new Vector3(11f, -2.34f, 0f), Quaternion.identity);
+        Instantiate(cars[2], new Vector3(-11f, -3.5f, 0f), Quaternion.identity);
+        carSpawnTimer = Time.time;
+    }
+}
